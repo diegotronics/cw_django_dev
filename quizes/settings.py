fb95d51b7421b2ed29cb4fe51a34e5005f0cba0f
Diego Perez <dperez@darient.com>
2023-11-21 23:54:22 -0400

    add dj_database_url

--- quizes/settings.py
+++ quizes/settings.py
@@ -13,6 +13,8 @@ import logging
 import os
 from pathlib import Path
 
+import dj_database_url
+
 STAGE = os.environ.get("STAGE", "local")
 logging.info(f"STAGE: {STAGE}")
 
@@ -87,12 +89,15 @@ WSGI_APPLICATION = "quizes.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if STAGE != "local" and os.environ.get("DATABASE_URL"):
+    DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
 
 
 # Password validation
